cf7463f10d7ece1bc32d3011df508f2c0f9656f8
hangxigood <hangxigood@gmail.com>
2024-11-29 21:23:58 -0700

    use next.auth's DefaultUser and DefaultJWT to do type augmentation

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,25 @@
 import { authOptions } from "../../../../lib/authOptions";
 import NextAuth from "next-auth/next";
-import { SessionUser } from "@/types/user";
+import { DefaultJWT } from "next-auth/jwt";
+import { DefaultUser } from "next-auth";
 
 /**
  * Type augmentation for NextAuth.js
  */
 declare module "next-auth" {
-  interface User extends SessionUser {}
+  interface User extends DefaultUser {
+    firstName: string;
+    lastName: string;
+    role: string;
+    phone?: string;
+    accessToken?: string;
+  }
 
   /**
    * Extends the Session interface to include our custom User type
    */
   interface Session {
-    user: SessionUser;
+    user: User;
   }
 }
 
@@ -20,7 +27,13 @@ declare module "next-auth" {
  * Type augmentation for NextAuth.js JWT
  */
 declare module "next-auth/jwt" {
-  interface JWT extends Omit<SessionUser, 'accessToken'> {}
+  interface JWT extends DefaultJWT {
+    sub: string;
+    firstName: string;
+    lastName: string;
+    role: string;
+    phone?: string;
+  }
 }
 
 export const dynamic = 'force-dynamic';
